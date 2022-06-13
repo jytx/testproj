f9b8a3d73365bdd31f0bb0475a42206449de726a
李群 <477939838@qq.com>
2022-06-13 15:12:38 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <view class="index">
+    <text @tap="sayHello">{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import './index.scss'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  },
+  methods:{
+    async sayHello(){
+      await new Promise((resolve, reject) => {
+        resolve()
+      })
+    }
+  }
+}
+</script>
